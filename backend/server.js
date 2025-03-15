13d10feec056bb51b82e6ced5e515963f1950e5c
biancadragan <andreeabiancadragan@gmail.com>
2025-03-15 14:53:12 +0100

    Connect server to MySQL & add /maps endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
 // Import libraries
 
 const express = require('express');
-const cors = require ('cors');
-const app = express();
+const cors = require('cors');
+require("dotenv").config();
+const mysql = require("mysql2/promise");
 
+async function getConnection() {
+    const connectionData = {
+        host: process.env["MYSQL_HOST"],
+        port: process.env["MYSQL_PORT"],
+        user: process.env["MYSQL_USER"],
+        password: process.env["MYSQL_PASS"],
+        database: process.env["MYSQL_SCHEMA"],
+    };
+    const connection = await mysql.createConnection(connectionData);
+    await connection.connect();
+
+    return connection;
+}
+
+//Server configuration
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
@@ -11,10 +29,22 @@ app.use(express.json());
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Example app listening on port <http://localhost:${port}>`);
+    console.log(`Example app listening on port <http://localhost:${port}>`);
 });
 
 //First endpoint
-app.get("/", (req, res) => {
-    res.send("MapAlbum backend is running");
-  });
+app.get("/maps", async (req, res) => {
+
+    const connection = await getConnection();
+
+    const [results] = await connection.query(`SELECT * FROM maps;`);
+
+    await connection.end();
+
+    const numberOfElements = results.length;
+
+    res.json({
+        info: { count: numberOfElements },
+        results: results,
+    });
+});
